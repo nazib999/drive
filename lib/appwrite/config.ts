520a9952e5d53a288fdd1e833c40ad3ab5122757
nazibullah <najibullahnajib758@gmail.com>
2025-07-30 09:02:05 +0600

    fixed all

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,5 +1,5 @@
 export const appwriteConfig = {
-    endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1',
+    endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://fra.cloud.appwrite.io/v1',
     projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '',
     databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE || '',
     usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION || '',
